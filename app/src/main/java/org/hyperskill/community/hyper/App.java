05c63d70d7082b77aaf2059322d3ca6c45bf4e59
Tanish Azad <tanishazad03@gmail.com>
2023-12-27 05:13:37 +0530

    init

---
+++ app/src/main/java/org/hyperskill/community/hyper/App.java
@@ -0,0 +1,11 @@
+package org.hyperskill.community.hyper;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
